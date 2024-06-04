d6afb0e549fc69bb00eea46dcaa95efac9668cbb
anobee <HP@DESKTOP-KFAVCD8>
2024-06-04 15:09:10 +0700

    membuat fungsi try

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -5,5 +5,15 @@
 //untuk objek array yang akan kita gunakan 
 using namespace std ;
 int main() {
+    cout << "awal program" << endl; //penanda 1.....
+    try {
+        array<int, 3> data = {3, 5, 7};
+        //pesan array integer 3 elemen 
+        cout << data.at(2)<<endl;
+        //memanggil array elemen ke 5
+    }
+    catch (exception& e) {
+        
+    }
     
 }
