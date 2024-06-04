bf526b5c423a2de493aa99844540bdba3657db92
anobee <HP@DESKTOP-KFAVCD8>
2024-06-04 15:06:01 +0700

    membuat exception

---
+++ exceptionStandard.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <exception>
+//untuk obyek exceprion yang akan kita gunakan
+#include <array>
+//untuk objek array yang akan kita gunakan 
+using namespace std ;
+int main() {
+    
+}
