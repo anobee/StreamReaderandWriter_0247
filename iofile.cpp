23194f3094c2459ccdb4e3e4993a5c99a3b56a38
anobee <HP@DESKTOP-KFAVCD8>
2024-06-04 14:37:12 +0700

    membuat program utama pada iofile

---
+++ iofile.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main () {
+    string baris;
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">=menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited 
+}
